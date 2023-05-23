658ecb4d00bd67b548219fa9fc43c102e3a03d07
Eongyu Park <peg5325@gmail.com>
2023-05-23 16:51:23 +0900

    practice front-controller logic

---
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -0,0 +1,46 @@
+package org.example.mvc;
+
+import org.example.mvc.controller.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @WebServlet : 어떤 경로로 요청이 들어와도 DispatcherServlet이 실행됨
+ */
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
+
+    private RequestMappingHandlerMapping rmhm;
+
+    @Override
+    public void init() throws ServletException {
+        rmhm = new RequestMappingHandlerMapping();
+        rmhm.init();
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("[DispatcherServlet] Service started.");
+
+        try {
+            Controller handler = rmhm.findHandler(request.getRequestURI());
+            String viewName = handler.handleRequest(request, response);
+
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+            requestDispatcher.forward(request, response);
+        } catch (Exception e) {
+            log.error("exception occurred: [{}]", e.getMessage(), e);
+            throw new ServletException(e);
+        }
+    }
+}
